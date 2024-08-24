8c942365b6cf23b9c640bc520cc4613a130b52bc
Omar Kraidié <omppula_1@hotmail.com>
2024-08-24 09:35:06 +0300

    fix test.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,11 @@ import type { Config } from 'tailwindcss';
 const config = {
 	darkMode: ['class'],
 	content: [
-		'./pages/**/*.{ts,tsx}',
-		'./components/**/*.{ts,tsx}',
-		'./app/**/*.{ts,tsx}',
-		'./src/**/*.{ts,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./Components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,ts,jsx,tsx}',
 	],
 	prefix: '',
 	theme: {
